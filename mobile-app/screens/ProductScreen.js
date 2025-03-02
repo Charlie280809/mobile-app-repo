faf637a95232d907822c3ffc7385e40821695017
Charlie Van Belle <r0995521@student.thomasmore.be>
2025-03-02 20:25:31 +0100

    [Props & States]-[removed // to reveal 'comments'-code]

--- mobile-app/screens/ProductScreen.js
+++ mobile-app/screens/ProductScreen.js
@@ -4,16 +4,17 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import ProductCard from '../components/ProductCard';
 
-const ProductScreen = ({ route, useState, navigation }) => { //functie die de homepagina van de app weergeeft
+const ProductScreen = ({ route, navigation }) => { //functie die de homepagina van de app weergeeft
   const { title, price, image, description } = route.params || {}; //haal de waarden uit de route.params
   
-  // const [quantity, setQuantity] = useState(1); //maak een state aan voor de hoeveelheid van het product (deze state staat standaard op 1)
-  // const increaseQuantity = () => setQuantity(quantity + 1); //functie die de hoeveelheid verhoogt
-  // const decreaseQuantity = () => {
-  //   if(quantity > 1) {
-  //     setQuantity(quantity - 1); //functie die de hoeveelheid verlaagt
-  //   }
-  // };
+  const [quantity, setQuantity] = useState(1); //maak een state aan voor de hoeveelheid van het product (deze state staat standaard op 1)
+  
+  const increaseQuantity = () => setQuantity(quantity + 1); //functie die de hoeveelheid verhoogt
+  const decreaseQuantity = () => {
+    if(quantity > 1) {
+      setQuantity(quantity - 1); //functie die de hoeveelheid verlaagt
+    }
+  };
 
   return (
         <View style={styles.container}>
@@ -23,7 +24,7 @@ const ProductScreen = ({ route, useState, navigation }) => { //functie die de ho
             <Text style={styles.price}>{price}</Text>
             <Text style={styles.description}>{description}</Text>
 
-            {/* <View style={styles.quantityContainer}>
+            <View style={styles.quantityContainer}>
               <TouchableOpacity onPress={decreaseQuantity}>
                 <Text style={styles.quantityButton}>-</Text>
               </TouchableOpacity>
@@ -33,8 +34,9 @@ const ProductScreen = ({ route, useState, navigation }) => { //functie die de ho
               <TouchableOpacity onPress={increaseQuantity}>
                 <Text style={styles.quantityButton}>+</Text>
               </TouchableOpacity>
-            </View> */}
+            </View>
 
+            <Text style={styles.totalPrice}>Totaal: €{price * quantity}</Text>
             <StatusBar style="auto" />
         </View>
     );
@@ -76,5 +78,30 @@ const styles = StyleSheet.create({
     marginTop: 5,
     padding: 20,
     textAlign: 'center',
+  },
+  quantityContainer: {
+    flexDirection: 'row',
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  quantityButton: {
+    fontSize: 24,
+    color: '#444',
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: '#ededed',
+  },
+  quantityText: {
+    fontSize: 24,
+    color: '#444',
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: '#ededed',
+    marginHorizontal: 10,
+  },
+  totalPrice: {
+    fontSize: 18,
+    color: '#444',
+    marginTop: 5
   },
 });
